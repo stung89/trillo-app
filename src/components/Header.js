eba626f0a438f07f49bcbfaf4f88d088f4a09080
Steven Tung <stung89@gmail.com>
2018-05-20 15:51:24 -0700

    add dropdowns

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,6 +10,13 @@ export const Header = ({ startLogout }) => (
         
         <form action="#" className="search">
             <input type="text" className="search__input" placeholder="Search hotels"/>
+            <ul className="search__dropdown">
+                <li className="search__dropdown--item">suggestion 1</li>
+                <li className="search__dropdown--item">suggestion 2</li>
+                <li className="search__dropdown--item">suggestion 3</li>
+                <li className="search__dropdown--item">suggestion 4</li>
+                <li className="search__dropdown--item">suggestion 5</li>
+            </ul>
             <button className="search__button">
                 <Icon name="icon-magnifying-glass" />
             </button>
@@ -19,10 +26,22 @@ export const Header = ({ startLogout }) => (
             <div className="user-nav__icon-box">
                 <Icon name="icon-bookmark" />
                 <span className="user-nav__notification">7</span>
+                <ul className="user-nav__dropdown">
+                    <li className="user-nav__list">choice 1</li>
+                    <li className="user-nav__list">choice 2</li>
+                    <li className="user-nav__list">choice 3</li>
+                    <li className="user-nav__list">choice 4</li>
+                </ul>
             </div>
             <div className="user-nav__icon-box">
                 <Icon name="icon-chat" />
                 <span className="user-nav__notification">13</span>
+                <ul className="user-nav__dropdown">
+                    <li className="user-nav__list">choice 1</li>
+                    <li className="user-nav__list">choice 2</li>
+                    <li className="user-nav__list">choice 3</li>
+                    <li className="user-nav__list">choice 4</li>
+                </ul>
             </div>
             <div className="user-nav__user">
                 <img src="images/user.jpg" alt="User photo" className="user-nav__user-photo"/>
